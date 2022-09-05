b1cce245dcd07c9ca9548968fbcdbf4d61fa62e2
Aaron <aguo@mawer.com>
2022-09-04 22:25:32 -0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'cherryPink':{
+        100: '#FCC3D4',
+        200: '#F988AA',
+        300: '#F53971'
+      } ,
+      'cherryGrey': {
+        50: '#FAFAFA',
+        100: '#F3F3F3',
+        200: '#D4D4D4',
+        300: '#AEAEAE',
+        400: '#343434'
+      },
+      'white':"#FFFFFF",
+      'blue':"#5A9AFF",
+
+    }
+  },
+}
